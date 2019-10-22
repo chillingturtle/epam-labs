0461bb0fd81ff289bbc512c372204267ff08575a
chillingturtle <ikovaliov28@gmail.com>
2019-10-22 08:59:59 +0300

    edit gitignore, add Testing, xUnit labs

---
+++ TriangleProj/TriangleProj/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriangleProj
+{
+    public class Triangle
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        private Triangle() { }
+
+        public static bool CheckIsPossibleToCreate(double a, double b, double c)
+        {
+            return (a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b);
+        }
+    }
+}
