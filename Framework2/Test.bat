b984f2ca76fa7cc99f110d928a40901e76261213
chillingturtle <ikovaliov28@gmail.com>
2020-01-21 04:11:12 +0300

    asdasd

--- Framework2/Test.bat
+++ Framework2/Test.bat
@@ -1,4 +1,4 @@
 cd ./UnitTestProject
 msbuild.exe UnitTestProject.csproj -property:Configuration=Debug
 cd ../packages\NUnit.ConsoleRunner.3.10.0\tools
-nunit3-console.exe --testparam:browser=Edge --testparam:environment=dev "../../../UnitTestProject/bin/Debug/UnitTestProject.dll" --where "cat=All"
+nunit3-console.exe --testparam:browser=Chrome --testparam:environment=dev "../../../UnitTestProject/bin/Debug/UnitTestProject.dll" --where "cat=All"
